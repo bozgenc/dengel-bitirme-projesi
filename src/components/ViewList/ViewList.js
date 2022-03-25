00b9d6ac432622356bab052c6f6d7989942e09c5
CanKocyigitoglu <kocyigitoglucan@gmail.com>
2022-03-25 18:30:52 +0300

    View List page is added

---
+++ src/components/ViewList/ViewList.js
@@ -0,0 +1,178 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    ScrollView,
+    TextInput,
+    Button,
+    TouchableOpacity,
+    Image,
+    Alert,
+    FlatList
+} from 'react-native';
+import {Header, Left, Right} from "native-base"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import DeviceInfo from "react-native-device-info";
+
+var screen = Dimensions.get('window');
+
+
+export default class MainScreenUser extends Component {
+    constructor() {
+        super();
+        this.state = {
+            meetingsIncoming: []
+        }
+    }
+
+    componentDidMount =  async () => {
+        let meetings = [
+            {
+                id: 0,
+                name: 'Baran Özgenç',
+                profession: 'Duygusal Sağlık',
+                price: '2,100.00'
+            },
+            {
+                id: 1,
+                name: 'Elif Afşar',
+                profession: 'Anksiyete',
+                price: '3,000.00'
+            },
+            {
+                name: 'Can Koçyiğitoğlu',
+                profession: 'Şizofreni',
+                price: '1,100.00'
+            }
+        ];
+
+        this.setState({
+            meetingsIncoming: meetings
+        })
+
+        try {
+            const response = await fetch("http://localhost:5000/records");
+            const jsonData = await response.json();
+            console.log(jsonData);
+        }
+        catch (e) {
+            console.log(e.message);
+        }
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1, backgroundColor: "#faf8f8"}}>
+                <View>
+                    <Header style={{backgroundColor: 'white', borderBottomWidth: 2, borderBottomColor: '#f18a21'}}>
+                        <Left>
+                            <TouchableOpacity
+                                onPress={() => this.props.navigation.openDrawer()}
+                                style={{color: "black"}}
+                            >
+                                <Text style={{marginLeft: 10, fontSize: 30, color: '#B00D23'}}>
+                                    ≡
+                                </Text>
+                            </TouchableOpacity>
+                        </Left>
+
+                        <Text style={{marginTop: 10, fontSize: 30, fontFamily: "Helvetica-Bold"}}>Liste</Text>
+
+                        <Right>
+                            <TouchableOpacity
+                                onPress={() => console.log(this.state.meetingsIncoming)}
+                                style={{color: "black"}}
+                            >
+                                <Text style={{marginLeft: 10, fontSize: 33, color: '#B00D23', marginRight: 6}}>
+                                    +
+                                </Text>
+                            </TouchableOpacity>
+                        </Right>
+                    </Header>
+                </View>
+
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'flex-start',
+                    marginLeft: 4.3,
+                    marginRight: 10,
+                    height: screen.height,
+                    backgroundColor: '#faf8f8'
+                }}>
+                    <FlatList
+                        style = {{flex: 0}}
+                        initialNumToRender={3}
+                        directionalLockEnabled={true}
+                        showsVerticalScrollIndicator={true}
+                        showsHorizontalScrollIndicator={false}
+                        keyExtractor={(item) => item.id}
+                        data={this.state.meetingsIncoming}
+                        renderItem={({item}) => (
+                            <TouchableOpacity
+                                onPress={() => {
+                                    console.log("Clicked to meeting"); // burada meetinge gidecek
+                                }}
+                            >
+                                <View style={styles.arrayItem}>
+                                    <Text style={styles.textStyleList}>{"İsim: "} {item.name} </Text>
+                                    <Text style={styles.textStyleList}>{"Alanı: "} {item.profession} </Text>
+                                    <Text style={styles.textStyleList}>{"Ücret: "} {item.price} </Text>
+                                </View>
+                            </TouchableOpacity>
+                        )}/>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    welcomingCard: {
+        paddingVertical: 2,
+        paddingHorizontal: 20,
+        backgroundColor: '#efebeb',
+        borderRadius: 15,
+        height: 100,
+        width: screen.width * 96.6 / 100,
+        shadowColor: 'black',
+        shadowOpacity: .2,
+        shadowRadius: 5,
+        marginTop: 10,
+        marginLeft: 6,
+    },
+    textStyle: {
+        marginTop: 10,
+        marginLeft: 2,
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    textStyle2: {
+        marginTop: 10,
+        marginLeft: 2,
+        fontSize: 14,
+    },
+    arrayItem: {
+        marginTop: 10,
+        paddingVertical: 2,
+        paddingHorizontal: 15,
+        backgroundColor: '#efebeb',
+        borderRadius: 10,
+        height: 60,
+        width: screen.width * 96.6 / 100,
+    },
+    textStyleList: {
+        marginLeft: 4,
+        fontSize: 12,
+        fontWeight: 'bold'
+    },
+    textStyle2List: {
+        marginTop: 5,
+        marginLeft: 4,
+        fontSize: 12,
+        color: 'black'
+    }
+});
