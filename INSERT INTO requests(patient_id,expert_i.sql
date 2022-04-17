fb3efdc23c8f6ba26e65644aaf61a59542f22d88
LamyTheGoat <almeh2000@gmail.com>
2022-04-17 03:09:02 +0300

    SQL INSERT QUERRIES

---
+++ INSERT INTO requests(patient_id,expert_i.sql
@@ -0,0 +1,8 @@
+INSERT INTO requests(patient_id,expert_id,request,appointment,price,isprivate,isapproved) VALUES (5, 6, current_date(0), 22-02-16, 200, true, false)
+INSERT INTO requests(patient_id,expert_id,request,appointment,price,isprivate,isapproved) VALUES (3, 6, current_date(0), 22-02-11, 150, true, true)
+INSERT INTO requests(patient_id,expert_id,request,appointment,price,isprivate,isapproved) VALUES (1, 6, 12-02-16, 22-02-16, 130, false, false)
+INSERT INTO requests(patient_id,expert_id,request,appointment,price,isprivate,isapproved) VALUES (2, 4, current_date(0), 19-03-17, 240, true, false)
+INSERT INTO requests(patient_id,expert_id,request,appointment,price,isprivate,isapproved) VALUES (7, 4, current_date(0), 12-04-19, 220, false, true)
+
+INSERT INTO posts() VALUES ()
+
